91cad5041fd1bba3a52e71d2ba4ec1cd5e6a1409
Sagor <mojahidulislamsagor1234@gmail.com>
2026-05-21 09:52:42 +0600

    Newsletter section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Banner from "@/components/Banner";
 import ActionSection from "@/components/HomePage/ActionSection";
 import CarCard from "@/components/HomePage/CarCard";
 import FetuaredSection from "@/components/HomePage/FetuaredSection";
+import NewsLetter from "@/components/HomePage/NewsLetter";
 import Testimonial from "@/components/HomePage/Testimonial";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <FetuaredSection></FetuaredSection>
       <ActionSection></ActionSection>
       <Testimonial></Testimonial>
+      <NewsLetter></NewsLetter>
     </div>
   );
 }
