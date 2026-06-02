9e655deafed832ae271ba8d926da53e0bfe9525d
Sagor <mojahidulislamsagor1234@gmail.com>
2026-06-02 17:01:16 +0600

    Commit

--- src/components/HomePage/FetuaredSection.jsx
+++ src/components/HomePage/FetuaredSection.jsx
@@ -22,10 +22,12 @@ const FetuaredSection = async () => {
             instantly and start your journey without hassle.
           </p>
         </div>
-        <div className="max-w-300 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-18 px-6 md:px-0">
-          {cars.slice(0, 6).map((car, i) => (
-            <CarCard key={i} car={car}></CarCard>
-          ))}
+        <div className="px-6 md:px-0">
+          <div className="max-w-300 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-18">
+            {cars.slice(0, 6).map((car, i) => (
+              <CarCard key={i} car={car}></CarCard>
+            ))}
+          </div>
         </div>
         <Link href="/explore-cars">
           <button className="btn px-6 py-2 hover:bg-primary hover:text-white rounded-md mt-18 cursor-pointer">
